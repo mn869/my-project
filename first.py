98619a0ee9584b8ccd61f9d223e2d1627e33d62e
Mary Njambi <njambi.maree@gmail.com>
2025-03-04 14:59:30 +0300

    updated new changes

---
+++ first.py
@@ -0,0 +1,14 @@
+# Integer (whole number)
+age = 30
+# float (decimal number)
+height = 7.8945
+# string (text)
+name = "swagger"
+# boolean (true/false) 
+student = True
+
+# check DataType
+print(type(age))
+print(type(height))
+print(type(name))
+print(type(student))
