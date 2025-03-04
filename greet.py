3b466a9c069d3f3aac74adf2b6a793a17ebca4bc
Mary Njambi <njambi.maree@gmail.com>
2025-03-04 15:56:44 +0300

    updated greet.py

---
+++ greet.py
@@ -0,0 +1,8 @@
+# Defining a function
+def greet(name):
+    print("Hello, " + name + "! ")
+
+# Calling the function
+greet("Alice")  # Outputs: Hello, Alice! 
+greet("Bob")    # Outputs: Hello, Bob! 
+greet("Swagger") #Outputs: Hello,Swagger!
